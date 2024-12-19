c6bfac9e3b2542476c9ac2b52963732633bf2f54
noezou <151537155+noezou@users.noreply.github.com>
2024-12-20 02:27:28 +0900

    12-20　02-27

--- insert.php
+++ insert.php
@@ -14,27 +14,38 @@ $comment = $_POST['comment'];
 echo $name;
 
 //2. DB接続する
+$hostname = '';
+$dbname = '';
+$user = '';
+$pass = '';
+// MAMPでの動作確認用
+// $hostname='localhost';
+// $dbname='kadai';
+// $user='root';
+// $pass='root';
+
 try {
-    //Password注意。MAMP='root'　XAMPP=''
-    $pdo = new PDO('mysql:dbname=kadai; charset=utf8; host=localhost', 'root', 'root');
+  //Password注意。MAMP='root'　XAMPP=''
+  $pdo = new PDO('mysql:host=' . $hostname . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
 } catch (PDOException $e) {
-    exit('DBConnectError:' . $e->getMessage());
+  exit('DBConnectError:' . $e->getMessage());
 }
 
 //３．データ登録SQL作成
 
 // 1. SQL文を用意
-$stmt = $pdo->prepare("INSERT
+$stmt = $pdo->prepare(
+  "INSERT
                           INTO
                         gs_bm_table(id,b_name,b_url,b_comment,date)
                         VALUES(NULL, :name, :url, :comment, now())"
-                      );
+);
 
 //  2. バインド変数を用意
 //Integer（数値の場合 PDO::PARAM_INT)
 //String（文字列の場合 PDO::PARAM_STR) 念には念を入れて書いている。もっとわかりにくい変数名のときに、文字列だよ、と示している。
 // 悪意あるSQLを実行させないために、１クッション置いている
-$stmt->bindValue(':name', $name, PDO::PARAM_STR); 
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->bindValue(':url', $url, PDO::PARAM_STR);
 $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
 
@@ -42,13 +53,11 @@ $stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
 $status = $stmt->execute();
 
 // 4．データ登録処理後 成功した場合：true、失敗した場合：false
-if($status === false){
+if ($status === false) {
   //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
   $error = $stmt->errorInfo();
-  exit('ErrorMessage:'.$error[2]);
-}else{
+  exit('ErrorMessage:' . $error[2]);
+} else {
   // 5．index.phpへリダイレクト
-header('Location: index.php');
-
+  header('Location: index.php');
 }
-?>
--- select.php
+++ select.php
@@ -2,11 +2,21 @@
 require_once('funcs.php');
 
 //1.  DB接続
+$hostname = '';
+$dbname = '';
+$user = '';
+$pass = '';
+// MAMPでの動作確認用
+// $hostname='localhost';
+// $dbname='kadai';
+// $user='root';
+// $pass='root';
+
 try {
   //Password:MAMP='root',XAMPP=''
-    $pdo = new PDO('mysql:dbname=kadai; charset=utf8; host=localhost', 'root', 'root');
+  $pdo = new PDO('mysql:host=' . $hostname . ';dbname=' . $dbname . ';charset=utf8', $user, $pass);
 } catch (PDOException $e) {
-  exit('DBConnectError'.$e->getMessage());
+  exit('DBConnectError' . $e->getMessage());
 }
 
 //２．データ取得SQL作成 登録済みのものを持ってくるので攻撃気にしないで良い
@@ -14,58 +24,64 @@ $stmt = $pdo->prepare("SELECT * FROM gs_bm_table;");
 $status = $stmt->execute();
 
 //３．データ表示
-$view="";
-$s=" ； ";
-if ($status==false) {
+$view = "";
+$s = " ； ";
+if ($status == false) {
   //execute（SQL実行時にエラーがある場合）
   $error = $stmt->errorInfo();
-  exit("ErrorQuery:".$error[2]);
-
-}else{
+  exit("ErrorQuery:" . $error[2]);
+} else {
   //Selectデータの数だけ自動でループしてくれる
   //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
   // $stmt->fetch(PDO::FETCH_ASSOC) 1行ずつ取ってきて、$resultに入れる
-  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){
+  while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $view .= '<p>';
-    $view .= $result['id'] .$s. h($result['b_name']) .$s. h($result['b_url']) .$s. h($result['b_comment']).$s. $result['date'] ; // [.]は変数に追記するとき。ドットをつけないと上書きされる。
+    $view .= $result['id'] . $s . h($result['b_name']) . $s . h($result['b_url']) . $s . h($result['b_comment']) . $s . $result['date']; // [.]は変数に追記するとき。ドットをつけないと上書きされる。
     $view .= '</p>';
   }
-
 }
 ?>
 
 
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>ブックマーク（DB呼び出し⇒HTML表示）</title>
-<link rel="stylesheet" href="css/range.css">
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<style>div{padding: 10px;font-size:16px;}</style>
-</head>
-<body id="main">
-<!-- Head[Start] -->
-<header>
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">登録データ</a>
-      </div>
-    </div>
-  </nav>
-</header>
-<!-- Head[End] -->
 
-<!-- Main[Start] -->
-<div>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ブックマーク（DB呼び出し⇒HTML表示）</title>
+  <link rel="stylesheet" href="css/range.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    div {
+      padding: 10px;
+      font-size: 16px;
+    }
+  </style>
+</head>
+
+<body id="main">
+  <!-- Head[Start] -->
+  <header>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">登録データ</a>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- Head[End] -->
+
+  <!-- Main[Start] -->
+  <div>
     <div class="container jumbotron"><?= $view ?></div> <!-- echoの省略形 -->
-</div>
-<br>
-<h1>入力ページに戻る</h1>
-<div><a href="index.php">index.phpファイル</a>に戻ります</div>
-<!-- Main[End] -->
+  </div>
+  <br>
+  <h1>入力ページに戻る</h1>
+  <div><a href="index.php">index.phpファイル</a>に戻ります</div>
+  <!-- Main[End] -->
 </body>
+
 </html>
